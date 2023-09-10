bef54a4939df304d61e1b3384352eb4fb893ae4d
josmellbu <josmell.alex@gmail.com>
2023-09-10 14:49:47 -0500

    second commit

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,28 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { IconButton, Typography } from "@mui/material"
+import { JournalLayout } from "../layout/journalLayout"
+import { NothingSelectedView } from "../views/NothingSelectedView"
+import { NoteView } from "../views/NoteView"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
-    <Routes>
-      <Route path="/" element={ <JournalPage /> }/>
-      <Route path="/*" element={ <Navigate to="/" /> }/>
-    </Routes>
+    <JournalLayout>
+      {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur deleniti laudantium, minus esse eius hic expedita beatae ea ratione rem, sapiente culpa. Soluta pariatur ipsum rerum accusantium praesentium eligendi nulla.</Typography> */}
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
+    </JournalLayout>
   )
 }
